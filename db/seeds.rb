a722b20f0e01a6ba58f97960ff8a84718455d526
BarbaraBontempo <barbarabontempo94@gmail.com>
2020-07-05 14:50:41 -0400

    ignore, im just tryiing to learn git

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,7 +8,8 @@ u2 = User.create(name: "Sally", city: "NYC")
 
 c1 = Charity.create(name: "Save The World", city: "NYC", url: "www.savetheworld.com")
 
-r1 = Review.create(heading: "The best!", body: "This charity is AMAZING!", rating: 8.9, user_id: u1.id, charity_id: c1.id)
-r2 = Review.create(heading: "Awesome organization!", body: "This charity really saves the world!", rating: 9.5, user_id: u2.id, charity_id: c1.id)
+r1 = Review.create(heading: "The best!", body: "This charity is AMAZING!", rating: 8.5, user_id: u1, charity_id: c1.id)
+r2 = Review.create(heading: "Awesome organization!", body: "This charity really saves the world!", rating: 9.5, user_id: u2, charity_id: c1.id)
 
-binding.pry
+
+puts "DONE SEEDING!"
